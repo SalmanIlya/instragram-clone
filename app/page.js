7fbec6578c6d1423f8952d19ef1b8a0766d507b5
salamanilyas <salamanilyas101@gmail.com>
2023-11-30 19:46:55 +0500

    add-sidebar-in-app-folder

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
-import Link from 'next/link'
+// import Link from 'next/link'
 import React from 'react'
 import Home from './Components/Home/Home'
+import Sidebar from './Components/Sidebar/Sidebar'
 
 const page = () => {
   return (
-    <div>
+    <div className='homepage'>
+        <Sidebar/>
+
       <Home/>
     </div>
   )
