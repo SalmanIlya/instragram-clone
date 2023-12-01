3cb27ef53d141fe072b91df4ce7b298c40c24bb4
salamanilyas <salamanilyas101@gmail.com>
2023-12-01 17:08:55 +0500

    small-changes

--- app/Components/Home/Home.jsx
+++ app/Components/Home/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import "./Style.css"
+import Sidebar from '../Sidebar/Sidebar'
 
 const Home = () => {
   return (
     <div className='home-main'>
-      <h1>Home</h1>
+   <Sidebar/>
+   <div className='home'>
+<div className='home-box-1'>box-1</div>
+<div className='home-box-2'>box-2</div>
+
+    
+   </div>
     </div>
   )
 }
