b628e7c95e93e616eedf7b84c936c87299b2a02e
salamanilyas <salamanilyas101@gmail.com>
2023-12-02 17:37:37 +0500

    small-changes

--- app/Components/Home/Home.jsx
+++ app/Components/Home/Home.jsx
@@ -1,14 +1,74 @@
 import React from 'react'
 import "./Style.css"
 import Sidebar from '../Sidebar/Sidebar'
+import Navabr from '../TopNavbar/Navabr'
 
 const Home = () => {
   return (
     <div className='home-main'>
+   <Navabr/>
    <Sidebar/>
-   <div className='home'>
-<div className='home-box-1'>box-1</div>
-<div className='home-box-2'>box-2</div>
+   <div className='home '>
+<div className='home-box-1'>
+  {/* status */}
+  <div className='Status flex flex-row '>
+<div className='flex flex-col justify-center items-center mx-2 '>
+  <div className='images-bg'>
+  <div className='bg-red-100 h-14 w-14 rounded-full'></div>
+
+  </div>
+  <p>name </p>
+</div>
+<div className='flex flex-col justify-center items-center mx-2 '>
+  <div className='images-bg'>
+  <div className='bg-red-100 h-14 w-14 rounded-full'></div>
+
+  </div>
+  <p>name </p>
+</div>
+<div className='flex flex-col justify-center items-center mx-2 '>
+  <div className='images-bg'>
+  <div className='bg-red-100 h-14 w-14 rounded-full'></div>
+
+  </div>
+  <p>name </p>
+</div><div className='flex flex-col justify-center items-center mx-2 '>
+  <div className='images-bg'>
+  <div className='bg-red-100 h-14 w-14 rounded-full'></div>
+
+  </div>
+  <p>name </p>
+</div><div className='flex flex-col justify-center items-center mx-2 '>
+  <div className='images-bg'>
+  <div className='bg-red-100 h-14 w-14 rounded-full'></div>
+
+  </div>
+  <p>name </p>
+</div><div className='flex flex-col justify-center items-center mx-2 '>
+  <div className='images-bg'>
+  <div className='bg-red-100 h-14 w-14 rounded-full'></div>
+
+  </div>
+  <p>name </p>
+</div>
+<div className='flex flex-col justify-center items-center mx-2 '>
+  <div className='images-bg'>
+  <div className='bg-red-100 h-14 w-14 rounded-full'></div>
+
+  </div>
+  <p>name </p>
+</div><div className='flex flex-col justify-center items-center mx-2 '>
+  <div className='images-bg'>
+  <div className='bg-red-100 h-14 w-14 rounded-full'></div>
+
+  </div>
+  <p>name </p>
+</div>
+
+
+  </div>
+</div>
+<div className='home-box-2'></div>
 
     
    </div>
